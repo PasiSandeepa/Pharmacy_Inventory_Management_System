71d71bd5135948f88ab06df3939d7d57ffbaf6fe
PasiSandeepa <pasisandeepa456@gmail.com>
2025-11-05 22:51:01 +0530

    Create Suppliers dto Class

---
+++ src/main/java/model/dto/Suppliers.java
@@ -0,0 +1,18 @@
+package model.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Suppliers {
+    private Integer id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private LocalDate createdAt;
+}
