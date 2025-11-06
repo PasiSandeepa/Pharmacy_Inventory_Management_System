1f19016e2746d0191c4964891b496811c94cd24d
PasiSandeepa <pasisandeepa456@gmail.com>
2025-11-06 18:09:42 +0530

    Create AddEditMedicine Popup & fxml file

---
+++ src/main/java/controller/AddEditMedicineFormController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddEditMedicineFormController {
+
+    @FXML
+    private TextField txtBatchno;
+
+    @FXML
+    private TextField txtBrand;
+
+    @FXML
+    private TextField txtExpireDate;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtQuantity;
+
+    @FXML
+    private TextField txtUnitPrice;
+
+    @FXML
+    void btnCancelOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEditOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
