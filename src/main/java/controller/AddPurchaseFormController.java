26ee1a50412ce3e75201188c1d122c2975dd8049
PasiSandeepa <pasisandeepa456@gmail.com>
2025-11-07 13:07:49 +0530

    AddPurchaseForm.fxml file create and AddPurchaseFormController class create

---
+++ src/main/java/controller/AddPurchaseFormController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddPurchaseFormController {
+
+    @FXML
+    private TextField txtInvoiceno;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPurchasedate;
+
+    @FXML
+    private TextField txtSupplierid;
+
+    @FXML
+    private TextField txtcretaedat;
+
+    @FXML
+    private TextField txttotalamount;
+
+    @FXML
+    void btnCancelOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEditOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
