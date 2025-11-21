b3d92c5b3825b6ca5020d79dfc76475532408ccb
PasiSandeepa <pasisandeepa456@gmail.com>
2025-11-21 11:38:22 +0530

    AddToCart funtion and data load table function create.

--- src/main/java/controller/BillingFormController.java
+++ src/main/java/controller/BillingFormController.java
@@ -6,18 +6,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import model.dto.CartItem;
 import model.dto.Medicine;
-import service.MedicineService;
-import service.UserService;
-import service.impl.MedicineServiceImpl;
-import service.impl.UserServiceImpl;
+import service.BillingService;
+import service.impl.BillingServiceImpl;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class BillingFormController implements Initializable {
 
-    MedicineService medicineService = new MedicineServiceImpl();
+    BillingService billingService = new BillingServiceImpl();
 
     @FXML
     private ComboBox<String> cmbCashier;
@@ -38,16 +40,20 @@ public class BillingFormController implements Initializable {
     private TableColumn<?, ?> colUnitPrice;
 
     @FXML
+    private TableColumn<?, ?> colCashier;
+
+    @FXML
     private Label lblDiscount;
 
     @FXML
     private Label lblFinalTotal;
 
-    @FXML
-    private Label lblSubtotal;
 
     @FXML
-    private TableView<?> tblBilling;
+    private TextField txtSaleid;
+
+    @FXML
+    private TableView<CartItem> tblBilling;
 
     @FXML
     private TextField txtCustomerName;
@@ -62,14 +68,99 @@ public class BillingFormController implements Initializable {
     private TextField txtUnitPrice;
 
 
+    @FXML
+    private Label lblSubTotal;
+
+
+    @FXML
+    void MedicineOnKeyReleased(KeyEvent event) {
+        String name = txtMedicineName.getText() == null ? "" : txtMedicineName.getText().trim();
+        if (name.isEmpty()) {
+
+            return;
+        }
+        Medicine medicine = billingService.searchByName(name);
+        if (medicine != null) {
+            txtUnitPrice.setText(String.valueOf(medicine.getUnit_price()));
+        } else {
+            txtUnitPrice.clear();
+        }
+    }
 
     @FXML
     void btnAddToCart(ActionEvent event) {
 
+        if (txtCustomerName.getText().isEmpty()) {
+            show(Alert.AlertType.ERROR, "Error", "Customer Name is required");
+            return;
+        }
+
+        ObservableList<CartItem> cartItems = tblBilling.getItems();
+
+        String medName = txtMedicineName.getText();
+        String customerName = txtCustomerName.getText();
+        int qty = Integer.parseInt(txtQty.getText());
+        double unitPrice = Double.parseDouble(txtUnitPrice.getText());
+        double total = calculateTotal(qty, unitPrice);
+
+
+        for (CartItem item : cartItems) {
+            if (item.getMedicineName().equalsIgnoreCase(medName) &&
+                    item.getCustomerName().equalsIgnoreCase(customerName)) {
+
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Duplicate Item");
+                alert.setHeaderText("This medicine is already in the cart for this customer!");
+                alert.setContentText("Do you want to update the quantity?");
+
+                Optional<ButtonType> option = alert.showAndWait();
+
+                if (option.get() == ButtonType.OK) {
+
+                    int newQty = item.getQuantity() + qty;
+                    item.setQuantity(newQty);
+
+                    item.setTotal(newQty * item.getUnitPrice());
+
+                    tblBilling.refresh();
+                    calculateFinalyTotal();
+                    clearFields();
+                    return;
+
+                } else {
+                    return;
+                }
+            }
+        }
+        CartItem newItem = new CartItem(
+                txtCustomerName.getText(),
+                medName,
+                unitPrice,
+                qty,
+                total,
+                cmbCashier.getValue()
+        );
+
+        cartItems.add(newItem);
+
+        tblBilling.setItems(cartItems);
+        calculateFinalyTotal();
+        clearFields();
     }
+
+
+    private double calculateTotal(int qty, double unitPrice) {
+        return qty * unitPrice;
+    }
+
 
     @FXML
     void btnExportPDFOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
 
     }
 
@@ -78,38 +169,48 @@ public class BillingFormController implements Initializable {
 
     }
 
-    public void MedicineOnKeyReleased(javafx.scene.input.KeyEvent keyEvent) {
-        String text = txtMedicineName.getText().trim();
-        if (text.isEmpty()) {
-            return;
-        }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> CmbList = FXCollections.observableArrayList();
+        CmbList.addAll("Cashier A", "Cashier B", "Cashier C", "Cashier D", "Cashier E");
+        cmbCashier.setItems(CmbList);
 
-        Medicine medicine = medicineService.searchByName(text);
-        if (medicine != null) {
-            txtUnitPrice.setText(String.valueOf(medicine.getUnit_price()));
-        } else {
-            txtUnitPrice.clear();
-        }
+        colCustomerName.setCellValueFactory(new PropertyValueFactory<>("customerName"));
+        colMedicineName.setCellValueFactory(new PropertyValueFactory<>("medicineName"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("Quantity"));
+        colUnitPrice.setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        colTotal.setCellValueFactory(new PropertyValueFactory<>("total"));
+        colCashier.setCellValueFactory(new PropertyValueFactory<>("cashier"));
     }
 
-    private void showAlert(Alert.AlertType warning, String message) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Alert");
+    public void clearFields() {
+        txtCustomerName.clear();
+        txtMedicineName.clear();
+        txtQty.clear();
+        txtUnitPrice.clear();
+        cmbCashier.getSelectionModel().clearSelection();
+    }
+
+    public void show(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        ObservableList<String> Cmblist = FXCollections.observableArrayList();
-        Cmblist.add("Cashier A");
-        Cmblist.add("Cashier B");
-        Cmblist.add("Cashier C");
-        Cmblist.add("Cashier D");
-        Cmblist.add("Cashier E");
-        cmbCashier.setItems(Cmblist);
+    public void calculateFinalyTotal() {
+        ObservableList<CartItem> cartItems = tblBilling.getItems();
+        double subtotal = 0.0;
+        for (CartItem item : cartItems) {
+            subtotal += item.getTotal();
+        }
+       lblSubTotal.setText(String.format("%.2f", subtotal));
 
+        double discount = subtotal * 0.1;
+        lblDiscount.setText(String.format("%.2f", discount));
+
+        double finalTotal = subtotal - discount;
+        lblFinalTotal.setText(String.format("%.2f", finalTotal));
     }
 }
-
